663b1a2913f77ffd934f056c8d3ee2fbfe07946d
salvarg8 <salvarodriguez_91@hotmail.com>
2026-07-11 17:12:21 -0300

    hero refactor

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -13,9 +13,11 @@ const Hero = () => {
           <h2 className='text-2xl pt-4'>Especialista en Java, Spring Boot & React.</h2>
           <p className='text-xl pt-4'>
             Desarrollador Fullstack con una sólida base arquitectónica en el backend y capacidad para construir interfaces de usuario dinámicas. Actualmente cursando la carrera de Ingeniería en Software en la Universidad Siglo 21, me apasiona el ciclo de vida completo del desarrollo: desde el diseño de bases de datos relacionales y la creación de APIs seguras, hasta la implementación de experiencias interactivas en el cliente. Busco crear soluciones escalables y de alta calidad.</p>
-          <div className='p-10 flex justify-center gap-4'>
-            <a className='bg-[#527987] py-2 px-12 rounded-3xl hover:bg-[#3a5a6b] transition-colors duration-300 cursor-pointer'>Ver Proyectos</a>
-            <a className='bg-[#527987] py-2 px-12 rounded-3xl hover:bg-[#3a5a6b] transition-colors duration-300 cursor-pointer'>Contactar</a>
+          <div className='p-10 flex flex-col sm:flex-row sm:flex-wrap justify-center gap-4'>
+            <a className='bg-[#527987] py-2 px-8 sm:px-12 rounded-3xl hover:bg-[#3a5a6b] transition-colors duration-300 cursor-pointer text-center w-full sm:w-auto'
+            href='/#projects'>Ver Proyectos</a>
+            <a className='bg-[#527987] py-2 px-8 sm:px-12 rounded-3xl hover:bg-[#3a5a6b] transition-colors duration-300 cursor-pointer text-center w-full sm:w-auto' href='/#contacto'>Contactar</a>
+            <a className='bg-[#527987] py-2 px-8 sm:px-12 rounded-3xl hover:bg-[#3a5a6b] transition-colors duration-300 cursor-pointer text-center w-full sm:w-auto' href='/#cv'>Descargar CV</a>
           </div>
         </div>
 
